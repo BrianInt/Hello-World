1c7116e6e9d12ade2c392e95ae98dc23b57bfd81
roka <roka.ta.999@gmail.com>
2015-05-07 21:26:31 +0300

    create test project

---
+++ src/main/java/com/first/web/FirstBean.java
@@ -0,0 +1,23 @@
+package com.first.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+
+@ManagedBean
+@SessionScoped
+public class FirstBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
